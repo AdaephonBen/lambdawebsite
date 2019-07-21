b741784473a0c3a002aaba85150fa36d2aed0924
adaephonben <ma18btech11011@iith.ac.in>
2019-07-21 14:58:26 +0530

    Anime

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-The lambda website
-The lambda website reloaded
-The lambda website reloaded reloaded
+# Lambda
+
+This is the official repository for the Lambda website hosted at http://iith.dev. 
+
+## Dependencies
+1. The Bootstrap Grid
---
+++ index.js
@@ -0,0 +1,12 @@
+anime({
+    targets: '.main-heading',
+    opacity: 1,
+    duration: 1000,
+    easing: 'easeInOutSine',
+    translateY: {
+        value: -100,
+        duration: 500,
+        delay: 1000,
+        easing: 'easeInCubic'
+    }
+});
